49594201673510079770c2765a5b9a8199a9ac78
twr579 <timothy.reynolds@utexas.edu>
2024-08-30 15:20:08 -0500

    Add animations to headline

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,16 +1,36 @@
-import { Box, Slide, Typography, useScrollTrigger } from '@mui/material';
+import { Box, Grow, Slide, Typography, useScrollTrigger } from '@mui/material';
 import { useEffect, useRef, useState } from 'react';
 import useWindowDimensions from '../helpers/getWindowDimensions';
+import TextTransition, { presets } from 'react-text-transition';
 
+const TEXT1 = ['', '.'];
+const TEXT2 = ['OLD', 'EW']
 
 function Headline() {
     const { height } = useWindowDimensions();
     const [elementHeight, setElementHeight] = useState(0);
+    const [index, setIndex] = useState(0);
+    const [grow, setGrow] = useState(false);
     const ref = useRef<HTMLElement>();
 
     useEffect(() => {
         setElementHeight(ref.current?.clientHeight || 0);
     }, [])
+
+    useEffect(() => {
+        setIndex(0);
+        setGrow(false);
+        document.body.style.overflow = "hidden";
+        const timeoutId = setTimeout(
+            () => {
+                setIndex(1);
+                setGrow(true);
+                document.body.style.overflow = "auto";
+            },
+            3000, // 3 seconds
+        );
+        return () => clearTimeout(timeoutId);
+    }, []);
 
     const trigger = useScrollTrigger({ threshold: height * 0.8 - elementHeight - 16, disableHysteresis: true });
 
@@ -26,8 +46,16 @@ function Headline() {
             >
                 <Slide appear={false} direction='down' in={!trigger}>
                     <Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
-                        <Typography variant='h1'>TIMREY.NEWS</Typography>
-                        <Typography variant='h5'>or: how I learned to stop worrying and love the web</Typography>
+                        <Typography variant='h1'>
+                            TIMREY
+                            <TextTransition inline>{TEXT1[index]}</TextTransition>
+                            N
+                            <TextTransition inline>{TEXT2[index]}</TextTransition>
+                            S
+                        </Typography>
+                        <Grow in={grow} timeout={2000}>
+                            <Typography variant='h5'>or: how I learned to stop worrying and love the web</Typography>
+                        </Grow>
                     </Box>
                 </Slide>
             </Box>
