a91b90397ac374e9c1c68442ccc2273300b809cc
twr579 <timothy.reynolds@utexas.edu>
2024-09-19 17:45:59 -0500

    Add filter system to work page

--- src/router/work/Work.tsx
+++ src/router/work/Work.tsx
@@ -1,14 +1,23 @@
-import { Box, Chip, Grid2, Typography, useTheme } from "@mui/material";
+import { Box, Chip, Fade, Grid2, Typography, useTheme } from "@mui/material";
 import Grid from "./components/Grid";
 import thermometer from "../../images/thermometer.png";
 import BlinkingIcon from "../layout/BlinkingIcon";
 import ArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { useState } from "react";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+
+enum Labels {
+    Cpp = 'C++',
+    Embedded = 'Embedded',
+    JavaScript = 'JavaScript',
+}
 
 const projects = [
     {
         title: 'Thermometer',
         date: '08/24 - 09/24',
-        labels: ['C++', 'Embedded'],
+        labels: [Labels.Cpp, Labels.Embedded],
         image: thermometer,
         brief: 'A portable, lightweight thermometer based on the ATmega328P microcontroller.',
         description: `I was inspired to work on this project because of my lack of a thermometer to take on hiking/camping trips.
@@ -19,28 +28,60 @@ const projects = [
 ];
 
 function Work() {
+    const [filterLabels, setFilterLabels] = useState(Object.values(Labels) as Array<keyof typeof Labels>);
+
+    const handleClick = (label: keyof typeof Labels) => {
+        const selected = filterLabels.includes(label);
+
+        let newFilterLabels;
+        if (selected) {
+            newFilterLabels = filterLabels.filter((el) => { return el !== label });
+            setFilterLabels(newFilterLabels);
+        } else {
+            newFilterLabels = [...filterLabels];
+            newFilterLabels.push(label);
+        }
+        setFilterLabels(newFilterLabels);
+    };
+
     const theme = useTheme();
 
     return (
         <>
             <Typography variant='h1'>Work</Typography>
+            <Typography variant='h4'> Filter: {
+                (Object.values(Labels) as Array<keyof typeof Labels>).map(
+                    (label, index) => <Chip
+                        label={label}
+                        sx={{ mr: 1, mb: 1 }}
+                        variant={filterLabels.includes(label) ? "outlined" : "filled"}
+                        icon={filterLabels.includes(label) ? <CheckCircleOutlineIcon /> : <RadioButtonUncheckedIcon />}
+                        onClick={() => handleClick(label)}
+                        key={index}
+                    />
+                )
+
+            }
+            </Typography>
             {projects.map((project, index) =>
-                <Box sx={{ border: `solid 1px ${theme.palette.primary.main}`, p: 2, mt: 2 }} key={index}>
-                    <Grid2 container alignItems={'center'} justifyContent={'space-between'}>
-                        <Typography variant='h2'>{project.title}</Typography>
-                        <Typography variant='h4'>{project.date}</Typography>
-                    </Grid2>
-                    {project.labels.map((label, index) =>
-                        <Chip label={label} key={index} sx={{ mr: 1 }} />
-                    )}
-                    <Grid><img src={project.image} width={'75%'} /></Grid>
-                    <Typography variant="h5"><BlinkingIcon icon={<ArrowRightIcon />} />
-                        {project.brief}
-                    </Typography>
-                    <Typography variant="h5"><BlinkingIcon icon={<ArrowRightIcon />} />
-                        {project.description}
-                    </Typography>
-                </Box>
+                <Fade in={filterLabels.some(el => project.labels.includes(el as Labels))} key={index}>
+                    <Box sx={{ border: `solid 1px ${theme.palette.primary.main}`, p: 2, my: 1 }}>
+                        <Grid2 container alignItems={'center'} justifyContent={'space-between'}>
+                            <Typography variant='h2'>{project.title}</Typography>
+                            <Typography variant='h4'>{project.date}</Typography>
+                        </Grid2>
+                        {project.labels.map((label, index) =>
+                            <Chip label={label} key={index} sx={{ mr: 1 }} />
+                        )}
+                        <Grid><img src={project.image} width={'75%'} /></Grid>
+                        <Typography variant="h5"><BlinkingIcon icon={<ArrowRightIcon />} />
+                            {project.brief}
+                        </Typography>
+                        <Typography variant="h5"><BlinkingIcon icon={<ArrowRightIcon />} />
+                            {project.description}
+                        </Typography>
+                    </Box>
+                </Fade>
             )}
         </>
     );
